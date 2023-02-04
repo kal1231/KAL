628a764edd3375c814117f380ccce133a0562fe5
KALThon <115185627+kal1231@users.noreply.github.com>
2023-02-05 00:01:05 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,24 +1,23 @@
-from sample_config import Config
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+bot = telegram.Bot(token="5447331145:AAGa2c0VZfZ8bJFLsT_HEMID93NZbhFujFM")
 
-class Development(Config):
-    # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-    # the name to display in your alive message
-    ALIVE_NAME = "Your value"
-    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
-    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "Your value"
-    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "Your value"
-    TG_BOT_USERNAME = "Your value"
-    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
-    # command handler
-    COMMAND_HAND_LER = "."
-    # sudo enter the id of sudo users userid's in that array
-    SUDO_USERS = []
-    # command hanler for sudo
-    SUDO_COMMAND_HAND_LER = "."
+def start(update, context):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+def button(update, context):
+    query = update.callback_query
+    query.answer()
+    if query.data == '1':
+        query.edit_message_text(text="You chose option 1.")
+    elif query.data == '2':
+        query.edit_message_text(text="You chose option 2.")
+
+updater = Updater(token="YOUR_BOT_TOKEN", use_context=True)
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
+updater.start_polling()
